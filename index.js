4ae2da304ed6d6566de21c98729e690af2faaa7e
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2018-05-22 19:05:06 +0200

    created compression option

--- index.js
+++ index.js
@@ -29,44 +29,103 @@ async function question(string) {
     });
 }
 
-async function createMif(filename) {
+async function main() {
+    const compress = process.argv.includes("--compress");
+    const filename = await question("File Name: ");
+
+    console.log("Creating mif file...");
+    console.log("");
+    const mif = await createMif(filename, compress);
+
+    if (compress) {
+        fs.writeFileSync(`${filename}.mif`, mif.content);
+        fs.writeFileSync(`${filename}.colors.mif`, mif.colors);
+        console.log(`File was compressed to ${mif.compressionRate}% of the original size`);
+        console.log(`Created: ${filename}.mif`);
+        console.log(`Created: ${filename}.colors.mif`);
+    } else {
+        fs.writeFileSync(`${filename}.mif`, mif.content);
+        console.log(`Created: ${filename}.mif`);
+        if (mif.compressionRate < 100) {
+            console.log(`Use "npm start -- --compress" to compress file to ${mif.compressionRate}% of the original size`);
+        }
+    }
+}
+
+async function createMif(filename, compress) {
     const pixels = await getPixels(filename);
 
+    const normal = createContent(pixels);
+    const compressed = createColorPallete(pixels);
+
+    if (compress) {
+        return compressed;
+    } else {
+        return {
+            content: normal,
+            compressionRate: compressed.compressionRate,
+        };
+    }
+}
+
+function createContent(pixels) {
     const data = pixels.data;
 
     let content = [];
     for (let i = 0; i < data.length; i+=4) {
         content.push(`${i/4}\t:\t${getHex(data[i], data[i+1], data[i+2])};`);
     }
-    return `-- MIF file create by mif-creator
--- by Joren Vandeweyer
--- https://github.com/jorenvandeweyer/mif-creator
-WIDTH=${24};
-DEPTH=${content.length};
 
-ADDRESS_RADIX=UNS;
-DATA_RADIX=HEX;
+    return template(24, content);
+}
 
-CONTENT BEGIN
-    ${content.join("\n    ")}
-END;\n`;
+function createColorPallete(pixels) {
+    const data = pixels.data;
+
+    const colors = {};
+    let content_content = new Array(data.length/4);
+    let counter = 0;
+
+    for (let i = 0; i < data.length; i+=4) {
+        const hex = getHex(data[i], data[i+1], data[i+2]);
+        if (!(hex in colors)) {
+            colors[hex] = counter++;
+        }
+        const address = colors[hex];
+        content_content[i/4] = `${i/4}\t:\t${address};`;
+    }
+
+    const color_keys = Object.keys(colors);
+    const content_colors = color_keys.sort((a, b) => colors[a] - colors[b]).map((color, index) => `${index}\t:\t${color}`);
+
+    return {
+        content: template(color_keys.length.toString(2).length, content_content, "DEC"),
+        colors: template(24, content_colors),
+        compressionRate: parseInt(100*(color_keys.length*24 + data.length*color_keys.length.toString(2).length)/(data.length*24)),
+    }
 }
 
 function hexCheck(hex) {
-    if (hex.length === 1) hex = "0" + hex;
-    return hex;
+    return (hex.length === 1) ? "0" + hex : hex
 }
 
 function getHex(red, green, blue) {
     return hexCheck(red.toString(16)) + hexCheck(green.toString(16)) + hexCheck(blue.toString(16));
 }
 
-async function main() {
-    const filename = await question("File Name: ");
+function template(width, content, data_radix="HEX", address_radix="UNS") {
+    return `-- MIF file created by mif-creator
+-- by Joren Vandeweyer
+-- https://github.com/jorenvandeweyer/mif-creator
+WIDTH=${width};
+DEPTH=${content.length};
 
-    const content = await createMif(filename);
+ADDRESS_RADIX=${address_radix};
+DATA_RADIX=${data_radix};
 
-    fs.writeFileSync(`${filename}.mif`, content);
+CONTENT BEGIN
+    ${content.join("\n    ")}
+END;\n`;
 }
 
 if (require.main === module) {
